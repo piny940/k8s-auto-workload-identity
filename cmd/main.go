d2312adcf2c5b9ecce11641c49ef00507effd673
piny940 <83708535+piny940@users.noreply.github.com>
2024-09-30 22:46:55 +0900

    create workload identity api

--- cmd/main.go
+++ cmd/main.go
@@ -157,6 +157,13 @@ func main() {
 			os.Exit(1)
 		}
 	}
+	if err = (&controller.WorkloadIdentityReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "WorkloadIdentity")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
